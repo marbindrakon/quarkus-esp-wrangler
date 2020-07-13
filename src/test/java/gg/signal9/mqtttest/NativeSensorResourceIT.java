0a750454f8d8507f2edc60c5d49998c5359069d5
Andrew Austin <aaustin@redhat.com>
2020-07-12 23:25:12 -0500

    Remove default test cases

--- src/test/java/gg/signal9/mqtttest/NativeSensorResourceIT.java
+++
@@ -1,9 +0,0 @@
-package gg.signal9.mqtttest;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeSensorResourceIT extends SensorResourceTest {
-
-    // Execute the same tests but in native mode.
-}
